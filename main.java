4814bc6f002344e6e491ed42b862fe9ff1232edd
Benedicta01 <akinrulie.ben@gmail.com>
2025-04-16 19:55:35 +0100

    Add files via upload

--- main.java
+++ main.java
@@ -1,16 +1,31 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        boolean sunny = true;
-        boolean hot = true;
-
-        if (sunny) {
-            if (hot) {
-                System.out.println("Use cap & half sleeves");
-            } else {
-                System.out.println("Use half sleeves");
-            }
-        } else {
-            System.out.println("Take Rain-Coat for safety");
+        Scanner scn = new Scanner(System.in);
+        int day = scn.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
         }
     }
 }
